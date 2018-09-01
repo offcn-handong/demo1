60bbb324ec770c315cb7022e2d59acc458cb84d5
offcn <wangyi126hd@126.com>
2018-09-01 17:48:05 +0800

    1

--- src/main/java/com/example/demo1/Demo1Application.java
+++ src/main/java/com/example/demo1/Demo1Application.java
@@ -8,5 +8,6 @@ public class Demo1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo1Application.class, args);
+        System.out.println(111);
     }
 }
